0c236954d0cbe87967cc8b35394618aae99f4eeb
Christoph Muck <muckz11@gmail.com>
2017-09-05 22:50:30 +0200

    Add Example

--- Example/RxAlamofire/ViewController.swift
+++ Example/RxAlamofire/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import RxAlamofire
+import RxSwift
 
 class ViewController: UIViewController {
 
+    var requestMaker: RequestMaker!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let factory = RequestMakerFactory(baseUrl: "https://jsonplaceholder.typicode.com")
+        requestMaker = factory.createInstance()
+        
+        getUsers().asObservable()
+            .flatMap{ Observable.from($0) }
+            .subscribe(onNext: { print($0) })
+            .disposed(by: disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +32,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getUsers() -> Single<[User]> {
+        return requestMaker.request(RequestSpecification(
+            path: "/users", method: .get
+        )).decodeFromJson()
+    }
 }
 
+struct User: Codable {
+    
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+}
